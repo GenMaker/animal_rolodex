8a35ca341daf150360bac5f6ae09b87367fb933c
Panela Leung <leungpanela@gmail.com>
2019-12-03 05:09:14 +0000

    finished initial file set up

--- lib/animal_rolodex.rb
+++ lib/animal_rolodex.rb
@@ -1,6 +1,3 @@
-require "animal_rolodex/version"
+require_relative "./animal_rolodex/version"
+require_relative './animal_rolodex/cli'
 
-module AnimalRolodex
-  class Error < StandardError; end
-  # Your code goes here...
-end
---
+++ lib/animal_rolodex/cli.rb
@@ -0,0 +1,6 @@
+class AnimalRolodex::CLI
+  
+  def call
+    puts "Welcome to your animal rolodex!"
+  end
+end
